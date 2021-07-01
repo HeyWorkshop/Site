5cc75e5f72e62f727e156612e7d58de1d290d006
Antonio Cituk <tonycituk@gmail.com>
2021-07-01 01:35:59 -0500

    :sparkles: Mobile splide added!
    There is missing team's info. :cat2:

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,1 +1,13 @@
 console.log("Hello there! 👋🎈")
+
+// Creates the slider for the Team Section
+var SPLIDE_OPTIONS = {
+	autoplay: true,
+	interval: 2100,
+	type: "loop",
+	perPage: 1
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	new Splide('#workshop-team-slider', SPLIDE_OPTIONS).mount();
+});
